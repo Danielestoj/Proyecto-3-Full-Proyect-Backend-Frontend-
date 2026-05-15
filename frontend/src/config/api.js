8a8fffd3f04ff8afddea8cfe4de5ddd9e41ec2f5
Danielestoj <danielestoj@gmail.com>
2026-05-15 18:47:31 +0100

    añadiendo puerto

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,2 @@
-const API_URL = "proyecto-3-full-proyect-backend-frontend.railway.internal";
+const API_URL = "proyecto-3-full-proyect-backend-frontend-production.up.railway.app";
 export default API_URL
