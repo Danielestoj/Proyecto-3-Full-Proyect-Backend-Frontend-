f29a87f77ad8eb2cf86e964c118ae55276717310
Danielestoj <danielestoj@gmail.com>
2026-05-15 18:28:21 +0100

    Update API_URL to use internal Railway URL

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,2 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000' || proyecto-3-full-proyect-backend-frontend.railway.internal
+const API_URL = proyecto-3-full-proyect-backend-frontend.railway.internal
 export default API_URL
